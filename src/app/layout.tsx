5565750eadf85b338c11cc821174380e2a80078e
SimionFlorin <simiflorin122@gmail.com>
2026-03-05 05:03:14 +0200

    gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WRQV4KWP');`}
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-0ZX0QSW8R9"
           strategy="afterInteractive"
@@ -50,6 +57,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WRQV4KWP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Providers>
           {children}
           <Toaster />
